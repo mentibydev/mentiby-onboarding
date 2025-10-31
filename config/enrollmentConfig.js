a2ad290a242a5f955f1cd3daee0d745e8b8c75ab
mentibydev <mentiby.mentors@gmail.com>
2025-10-31 21:50:42 +0530

    Update enrollmentConfig.js

--- config/enrollmentConfig.js
+++ config/enrollmentConfig.js
@@ -2,9 +2,9 @@
 // Change this number to set the starting enrollment number after "MBY"
 // Example: If STARTING_ENROLLMENT_NUMBER = 2001, first ID will be 25MBY2001
 
-export const STARTING_ENROLLMENT_NUMBER = 3501;
-export const COHORT_TYPE = "Placement";
-export const COHORT_NUMBER = "3.0";
+export const STARTING_ENROLLMENT_NUMBER = 5000;
+export const COHORT_TYPE = "Basic";
+export const COHORT_NUMBER = "5.0";
 
 // You can change the above values as needed:
 // COHORT_TYPE examples: "Full Stack Development", "Basic", "Placement", "Mern"
